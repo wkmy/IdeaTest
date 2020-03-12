87c1cf0fc0f97824c4a7ffd113c3665a469bd44b
wkm <1048405015@qq.com>
2020-03-12 16:44:28 +0800

    第二次提交

---
+++ spring-boot-10-cloud-provider-ticket/src/main/java/com/example/springboot10cloudproviderticket/SpringBoot10CloudProviderTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot10cloudproviderticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10CloudProviderTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot10CloudProviderTicketApplication.class, args);
+    }
+
+}
---
+++ spring-boot-10-cloud-provider-ticket/src/main/java/com/example/springboot10cloudproviderticket/service/TicketService.java
@@ -0,0 +1,12 @@
+package com.example.springboot10cloudproviderticket.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+
+    public String getTicket(){
+        return "《七宗罪》";
+    }
+}
